f8d7a08322ea84b20661a96ec0750be3f619434c
BugHunter-X <askzenai@gmail.com>
2024-12-20 05:26:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+<div class="bg-gray-300 p-4 rounded-lg">
+  <h2 class="text-xl font-bold mb-2">Uncommon Tailwind CSS Bug</h2>
+  <p>This is a paragraph with some text.</p>
+  <p>There is no visible issue.</p>
+</div>
+```
